1a1240e10d835de4092fd4ae525e213989860b66
MH-Moon-1237 <mhmoon1237@gmail.com>
2023-02-06 21:54:18 +0600

    3rd

---
+++ OOP Lab Task/Name.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.name;
+
+/**
+ *
+ * @author MOON
+ */
+public class Name {
+
+    public static void main(String[] args) {
+        String Name =("Md Mahmudul Hasan Moon");
+        System.out.println(Name);
+    }
+}
