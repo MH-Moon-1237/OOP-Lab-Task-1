6366f696931f8b51ab1b31a5285836dd8be943ce
MH-Moon-1237 <mhmoon1237@gmail.com>
2023-02-06 21:56:06 +0600

    4th

---
+++ OOP Lab Task/AreaCircle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.areacircle;
+
+/**
+ *
+ * @author MOON
+ */
+public class AreaCircle {
+
+    public static void main(String[] args) {
+        double pi =3.1416,area;
+        int r=3;
+        area=pi*r*r;
+        System.out.printf("Area of Circle is : %.2f\n", area);
+    }
+}
