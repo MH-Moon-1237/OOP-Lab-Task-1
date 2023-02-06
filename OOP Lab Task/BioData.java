87c80d1bd10e32e55a7f94ba836651f011146854
MH-Moon-1237 <mhmoon1237@gmail.com>
2023-02-06 21:33:30 +0600

    1st

---
+++ OOP Lab Task/BioData.java
@@ -0,0 +1,15 @@
+
+
+package labproject.biodata;
+
+
+public class BioData {
+
+    public static void main(String[] args) {
+        System.out.println("Md Mahmudul Hasan Moon");
+         System.out.println("ID: 221-15-5246");
+          System.out.println("Ranisonkoil,Thakurgaon");
+           System.out.println("B.sc in CSE");
+           System.out.println("Favourite game: Cricket");
+    }
+}
