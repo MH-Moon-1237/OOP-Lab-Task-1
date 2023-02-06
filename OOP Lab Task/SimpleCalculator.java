9fa8deb22a928faeb2bbf99e7bffd36cc3a92fa3
MH-Moon-1237 <mhmoon1237@gmail.com>
2023-02-06 21:38:25 +0600

    2nd

---
+++ OOP Lab Task/SimpleCalculator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.simplecalculator;
+
+/**
+ *
+ * @author MOON
+ */
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        int a,b;
+        a=14;
+        b=7;
+        
+        int Summation = a+b;
+        int Subtraction = a-b;
+        int Multiplication = a*b;
+        int Division = a/b;
+        
+        System.out.println("Summation:" +Summation);
+        System.out.println("Subtraction:" +Subtraction);
+        System.out.println("Multiplication:" +Multiplication);
+        System.out.println("Division:" +Division); 
+    }
+}
