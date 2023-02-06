9a1e7b885092c46b5c20a391fa482728b85677fe
MH-Moon-1237 <mhmoon1237@gmail.com>
2023-02-06 21:57:24 +0600

    5th

---
+++ OOP Lab Task/EqucationSolve.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labproject.equcationsolve;
+
+/**
+ *
+ * @author MOON
+ */
+public class EqucationSolve {
+
+    public static void main(String[] args) {
+        int A=5;
+        int B=6;
+        int C=7;
+        int D=8;
+        
+        int result1= (A * B - C * D);
+        int result2= (2*A - B + 3*D);
+        int result3= (A*2 + B*2 - C*2 + D*2);
+        int result4= (A*3 + B - C*2);
+        
+        System.out.println("Result :" +result1);
+        System.out.println("Result :" +result2);
+        System.out.println("Result :" +result3);
+        System.out.println("Result :" +result4);
+    
+    }
+}
